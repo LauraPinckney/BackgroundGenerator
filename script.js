11ca2437d2f58da5e45090d9c8e3133128dd4599
Laura Pinckney <laura.pinckney@yahoo.com>
2024-01-29 10:03:12 +0000

    random scaffolding

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 
 function setGradient() {
+  console.log(color1.value, color2.value);
   body.style.background =
     'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
 
@@ -13,3 +14,10 @@ function setGradient() {
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
+
+window.addEventListener('load', setGradient);
+
+function generateRandomColour() {
+  const result = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${result}`;
+}
